cff584ebfacaf4d94d63ec819ef75950c667f687
g-poulos <cs04480@uoi.gr>
2022-03-11 21:40:41 +0200

    Added "Add Course" Form, redirect not working

--- src/main/java/_4352_4421_4480/springbootproject/Course/CourseController.java
+++ src/main/java/_4352_4421_4480/springbootproject/Course/CourseController.java
@@ -2,9 +2,7 @@ package _4352_4421_4480.springbootproject.Course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,10 +23,10 @@ public class CourseController {
         this.courseService = courseService;
     }
 
-//    @GetMapping
-//    public List<Course> getCourse() {
-//        return courseService.getCourse();
-//    }
+    @GetMapping
+    public List<Course> getCourse() {
+        return courseService.getCourse();
+    }
 
     @GetMapping({"/showCourses", "/", "/list"})
     public ModelAndView showCourses(Model model) {
@@ -36,5 +34,19 @@ public class CourseController {
         List<Course> listOfCourses = eRepo.findAll();
         mav.addObject("courses", listOfCourses);
         return mav;
+    }
+
+    @GetMapping("/addCourseForm")
+    public ModelAndView addCourse() {
+        ModelAndView mav = new ModelAndView("add-course-form");
+        Course newCourse = new Course();
+        mav.addObject("course", newCourse);
+        return mav;
+    }
+
+    @PostMapping("/addCourse")
+    public String addCourse(@ModelAttribute Course course) {
+        eRepo.save(course);
+        return "redirect:/list";
     }
 }
