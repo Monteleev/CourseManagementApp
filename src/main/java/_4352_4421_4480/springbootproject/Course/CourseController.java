0390f6ba07b66a644607190fd408713f49ba699c
Monteleev <cs04421@uoi.gr>
2022-04-10 20:14:31 +0300

    Fixed Update Course

--- src/main/java/_4352_4421_4480/springbootproject/Course/CourseController.java
+++ src/main/java/_4352_4421_4480/springbootproject/Course/CourseController.java
@@ -74,7 +74,6 @@ public class CourseController {
 
         // get student from database by id
         Course existingCourse = courseService.getCourseById(id);
-        existingCourse.setId(id);
         existingCourse.setName(course.getName());
         existingCourse.setSyllabus(course.getSyllabus());
         existingCourse.setYear(course.getYear());
@@ -87,7 +86,7 @@ public class CourseController {
 
     @PutMapping("/courses/{courseId}/students/{studentId}")
     String enrollStudentToCourse(@PathVariable Long courseId,
-                                 @PathVariable Integer studentId) {
+                                 @PathVariable Long studentId) {
         Course course = courseService.getCourseById(courseId);
         Student student = courseService.getStudentRepository().findById(studentId).get();
         course.enrollStudent(student);
