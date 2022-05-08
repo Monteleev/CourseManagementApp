d112b92f75ae47e2fee035fc6cf8abeecbb2bc22
g-poulos <cs04480@uoi.gr>
2022-05-08 21:44:55 +0300

    Added security

--- src/main/java/_4352_4421_4480/springbootproject/config/SecurityConfig.java
+++ src/main/java/_4352_4421_4480/springbootproject/config/SecurityConfig.java
@@ -3,10 +3,18 @@ package _4352_4421_4480.springbootproject.config;
 import _4352_4421_4480.springbootproject.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
@@ -18,25 +26,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // the boolean flags force the redirection even though
-        // the user requested a specific secured resource.
+//        // the boolean flags force the redirection even though
+//        // the user requested a specific secured resource.
         http.formLogin().defaultSuccessUrl("/courses", true);
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/courses/**").hasRole("INSTRUCTOR")
+                .antMatchers("/students/**").hasRole("INSTRUCTOR")
+                .antMatchers("/login*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().accessDeniedPage("/error.html");
 
-        // You can customize the following part based on your project, it's only a sample
-        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
-                .authenticated().and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
     }
 
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(courseService);
-//    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+        UserDetails user1 = User.withUsername("john")
+                .password(passwordEncoder().encode("1234"))
+                .roles("INSTRUCTOR")
+                .build();
+
+        UserDetails user2 = User.withUsername("mary")
+                .password(passwordEncoder().encode("iammary"))
+                .roles("USER")
+                .build();
+
+        auth.inMemoryAuthentication().withUser(user1).withUser(user2);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
 
