b3fbe288263c668a55a2d4a5f33f2d466ecd0261
Monteleev <cs04421@uoi.gr>
2022-05-10 15:32:53 +0300

    Added StatisticStrategyTest but not working

---
+++ src/test/java/_4352_4421_4480/springbootproject/entity/StatisticStrategyTest.java
@@ -0,0 +1,50 @@
+package _4352_4421_4480.springbootproject.entity;
+
+import _4352_4421_4480.springbootproject.service.CourseRatingService;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.entry;
+import static org.junit.Assert.assertEquals;
+
+public class StatisticStrategyTest {
+
+    Course course = new Course(1L,"SoftEng","courses syllabus",3,6);
+    Student student1 = new Student(1L, "Giwrgos", 2015);
+    Student student2 = new Student(2L, "Anna", 2010);
+    Student student3 = new Student(3L, "Petros", 2019);
+    Student student4 = new Student(4L, "Maria", 2018);
+
+    RatingId ratingId1 = new RatingId(course.getId(), student1.getId());
+    RatingId ratingId2 = new RatingId(course.getId(), student2.getId());
+    RatingId ratingId3 = new RatingId(course.getId(), student3.getId());
+    RatingId ratingId4 = new RatingId(course.getId(), student4.getId());
+
+
+    CourseRating courseRating1 = new CourseRating(ratingId1, course, student1, "5");
+    CourseRating courseRating2 = new CourseRating(ratingId2, course, student2, "6");
+    CourseRating courseRating3 = new CourseRating(ratingId3, course, student3, "7");
+    CourseRating courseRating4 = new CourseRating(ratingId4, course, student4, "8");
+
+    StatisticStrategy statisticStrategy = new StatisticStrategy();
+
+    @Test
+
+    public void calculateCourseStatisticsTest()
+    {
+
+        Map<String, Double> res = statisticStrategy.calculateCourseStatistics(course);
+
+        Map<String, Double> expectedRes = Map.ofEntries(entry("Mean",6.5),entry("Min",5.0),
+                entry("25th Percentile",5.25),entry("Max",8.0),entry("75th Percentile",7.75),
+                entry("Skewness",0.0),entry("Standard Deviation",1.2909944487358056),entry("Median",6.5),
+                entry("Kurtosis",-1.1999999999999993),entry("50th Percentile",6.5),entry("Variance",1.6666666666666667));
+
+        assertEquals(expectedRes,res);
+
+    }
+
+}
